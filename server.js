a5f63efa1e2aee8264e7586db5162782fa027ec4
danny-jin <danny@cabinetex.com>
2021-07-01 22:49:21 +0900

    update: server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = 80;
+const port = process.env.PORT || 80;
 app.use(express.static('./dist/airtm-front'));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname,'/dist/airtm-front/index.html'));
